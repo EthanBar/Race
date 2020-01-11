1c9f197c28b98330f438124d93fb94b6d78a23c4
Ethan Barker <ethanbarker0@gmail.com>
2020-01-10 22:57:48 -0800

    Outline Racer Implementation

--- Race/ui.py
+++ Race/ui.py
@@ -1,11 +1,17 @@
 import click
+import animals
 
+racers = {}
 
 @click.command()
 def race():
     while click.confirm("Would you like to register a racer?"):
-        # TODO - implement register
-        click.echo("register!")
+        name = click.prompt("What is your racer's name?")
+        speed = click.prompt("How fast is " + name + "?", type=int)
+        speed = click.prompt("How much does " + name + " weigh?", type=int)
+
+        # racers[name] = animals.Turtle
+
     while click.confirm("Would you like to run a race?"):
         # TODO -  implement race
         click.echo("race!")

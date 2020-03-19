543ba9c65d48c0b46941ffda8e86a1c94cdd7c29
Ethan Barker <ethanbarker0@gmail.com>
2020-03-19 16:09:21 -0700

    Added tests for trackphysics.py

--- test/test_trackphysics.py
+++ test/test_trackphysics.py
@@ -1,5 +1,5 @@
 import unittest
-from Race import trackphysics
+from Race import trackphysics, animals
 
 
 class Tests(unittest.TestCase):
@@ -13,6 +13,21 @@ class Tests(unittest.TestCase):
         track.points = [0, 1]
         track.length = 1
         self.assertEqual(trackphysics.Track._find_slope(track, 0.5), 1)
+
+    def test_empty_racers(self):
+        try:
+            trackphysics.Track.run_race(trackphysics.Track([]), [], 1)
+        except:
+            self.fail("Empty array of racers caused track to raise exception")
+
+    def test_determine_time(self):
+        track = trackphysics.Track
+        track.points = [0, 1]
+        track.length = 1
+        animal = animals.Racer
+        animal.acceleration = 0.5
+        self.assertEqual(trackphysics.Track._determine_time(track, animal, 1), 5)
+
 
 
 if __name__ == '__main__':

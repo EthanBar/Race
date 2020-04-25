45acde3ac856d93d254f9cd8f75d14d081b72816
Ethan Barker <ethanbarker0@gmail.com>
2020-04-25 16:48:48 -0700

    Organized gui_main.py into a grid system and added headers to improve ease of use

--- Race/gui_main.py
+++ Race/gui_main.py
@@ -4,9 +4,9 @@ import trackphysics
 import gui_results
 
 
-class DropDown:
+class InputRow:
 
-    def __init__(self, options):
+    def __init__(self, options, position):
         self.animal = tkinter.StringVar(roster)
         self.animal.set(options[0])
 
@@ -15,14 +15,14 @@ class DropDown:
 
         self.dropdown = tkinter.OptionMenu(roster, self.animal, *options)
         self.dropdown.config(width=20, font=('Helvetica', 12))
-        self.dropdown.pack()
+        self.dropdown.grid(row=position, column=0)
 
         self.field = tkinter.Entry(roster)
-        self.field.pack()
+        self.field.grid(row=position, column=1)
 
 
 def add_choice():
-    racer_species.append(DropDown(species))
+    racer_species.append(InputRow(species, 3 + len(racer_species)))
 
 
 def run_race():
@@ -46,15 +46,19 @@ roster.title("Race Track")
 
 species = ["Dog", "Cat", "Turtle", "Rabbit", "Elephant"]
 
+roaster_header = tkinter.Label(roster, text="Race Controls", font='Helvetica 18 bold')
+roaster_header.grid(row=0, column=0, columnspan=2, pady=(10, 10))
+
 add_racer = tkinter.Button(
     text="Add Another Racer",
     fg="black",
     bg="white",
+    highlightbackground="blue",
     width=20,
     height=2,
     command=add_choice
 )
-add_racer.pack()
+add_racer.grid(row=1, column=0)
 
 start_race = tkinter.Button(
     text="Start Race!",
@@ -65,10 +69,10 @@ start_race = tkinter.Button(
     height=2,
     command=run_race
 )
-start_race.pack()
+start_race.grid(row=1, column=1)
 
-header = tkinter.Label(roster, text="Racer Roster")
-header.pack()
+roaster_header = tkinter.Label(roster, text="Racer Roster", font='Helvetica 18 bold')
+roaster_header.grid(row=2, column=0, columnspan=2, pady=(10, 10))
 
 racer_species = []
 add_choice()
